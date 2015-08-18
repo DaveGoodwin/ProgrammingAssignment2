1b247793cea178ce5774a664bc716b770df1bf99
Dave Goodwin <goodwindave@gmail.com>
2015-08-18 14:27:29 -0500

    committing my comments

--- cachematrix.R
+++ cachematrix.R
@@ -12,4 +12,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+        ## hello I am Dave Goodwin and I am going to submit this file to 
+        ## github.  I will likely not have time to complete this assignment,
+        ## but at least this way I will get partial credit.
+    
+       ## have a nice day!
 }
